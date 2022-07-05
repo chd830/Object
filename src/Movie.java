6786383eda6f5a7979731a54306c54a870972d51
chd830@naver.com <chd830@naver.com>
2022-07-05 17:50:28 +0900

    인스턴스 변수를 통해 할인 정책을 변경

--- src/Movie.java
+++ src/Movie.java
@@ -21,4 +21,8 @@ public class Movie {
     public Money calculateMovieFee(Screening screening) {
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
+
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 }
